319ede240e6dfc4607931de57c75628b6da58ca8
eremus <eremus97@hotmail.com>
2024-02-22 12:57:18 +0800

    create basic logic with gameover screen

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 let score = 0;
 let lives = 3;
 
-// let ninjas = document.querySelectorAll(".ninja");
-let ninjas = document.getElementById("n1");
+const shotSound = document.getElementById("shotmp3");
+const bgm = document.getElementById("bgm");
+const hitSound = document.getElementById("hitmp3");
 
-ninjas.addEventListener("click", shoot);
+const ninjas = document.querySelectorAll(".ninja");
 
-function shoot() {
-  ninjas.style.display = "none";
+ninjas.forEach((ninja) => {
+  ninja.addEventListener("click", shoot);
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("ninja")) {
+    miss(e);
+  }
+});
+
+function shoot(e) {
+  //   shotSound.play(); // Play the sound effect
+  const clickedNinja = e.target; // Get the specific clicked ninja
+  clickedNinja.style.display = "none"; // Hide the clicked ninja
+  hitSound.play(); // Play the sound effect
   updateScore();
+}
+
+function miss(e) {
+  shotSound.volume = 0.1;
+  shotSound.play();
+  loseLives();
+  if (lives === 0) {
+    alert(`Game Over! Your score is ${score}. Refresh to try again!`);
+  }
 }
 
 function updateScore() {
   score++;
-  Document.getElementById("score").innerHTML = `Current Score: ${score}`;
+  document.getElementById("score").innerHTML = `Current Score: ${score}`;
+}
+
+function loseLives() {
+  lives--;
+  document.getElementById("lives").innerHTML = `Current Lives: ${lives}`;
 }
